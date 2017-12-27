f41d57ebd67a0aed1fd908c7f828456d01e6e95e
roeef <roeef@mail-wise.com>
2017-12-27 15:41:33 +0200

    comment and user name

--- main.js
+++ main.js
@@ -13,17 +13,29 @@ function createPost() {
     post.val("");
 }
 
-const REMOVE_BUTTON_HTML ="<button class='btn btn-primary remove' type='button'>Remove</button>"
+const REMOVE_BUTTON_HTML ="<button class='btn btn-primary remove' type='button'>Remove</button>";
+const COMMENT_DIV = "<div class='comment-box'></div>";
+const COMMENT_INPUT = '<input type="text" class="form-control comment-content" placeholder="Comment">';
+const COMMENT_USER = '<input type="text" class="form-control comment-user" placeholder="userName">';
+const NEW_LINE ="<BR>";
+const COMMENT_BUTTON = "<button class='btn btn-primary comment' type='button'>Comment</button>";
 function render() {
     let postsDiv = $('.posts');
     postsDiv.empty();
 
     for (let i = 0; i<posts.length; i++) {
+        // create post <p>
         let post = $("<p class='post' data-id='"+posts[i].id+"'>"+ posts[i].text +"</p>");
-        let removeButton = $(REMOVE_BUTTON_HTML)
-        post.append(removeButton);
+        // create button
+        let removeButton = $(REMOVE_BUTTON_HTML);
+        let userName = $(COMMENT_USER);
+        let commentBox = $(COMMENT_DIV).append(userName).append(COMMENT_INPUT).append(COMMENT_BUTTON);
+        // append button to post
+        post.append(removeButton).append(NEW_LINE).append(commentBox);
+        // append post to all posts div
         postsDiv.append(post);
 
+        // on click remove from array and render again.
         removeButton.click(function(){
             posts.splice(i,1);
             render();
