4296a1c1bf3a968b8cf04631388763528bbb0c56
roeef <roeef@mail-wise.com>
2017-12-28 04:53:22 +0200

    extention !!!

--- main.js
+++ main.js
@@ -1,51 +1,100 @@
-// Out post object class with a running id
-var nextId = 1;
+// add comment function
+const REMOVE_BUTTON_HTML = "<button class='btn btn-default remove' type='button'>Remove Post</button>";
+
+const COMMENT_DIV = "<div class='comment-box'></div>";
+
+const COMMENTS_NAME = "<span class='comments-name'></span>";
+const COMMENTS_CONTENT = "<span class='comments-text'></span>";
+const REMOVE_COMMENT = "<a class='remove'>Remove</a>";
+
+const COMMENT_INPUT = '<input type="text" class="form-control comment-content" placeholder="Your Comment">';
+
+const COMMENT_USER = '<input type="text" class="form-control comment-user" placeholder="name">';
+
+const ROW = "<DIV class='row'></DIV>";
+
+const COMMENT_BUTTON = "<button class='btn btn-default comment-btn' type='button'>Post Comment</button>";
+
+
+// Our post object class with a running id
+let nextId = 1;
 function Post(text) {
-        this.id = nextId++;
-        this.text = text;
-    };
+    this.id = nextId++;
+    this.text = text;
+    // a comment array
+    this.comments = [];
+    this.addComment = function (name, comment) {
+        this.comments.push({"name": name, "comment": comment});
+    }
 
-posts=[];
+}
 
+posts = [];
+function onCreatePostClick() {
+    createPost();
+    render();
+
+
+}
 function createPost() {
     let post = $('#post-name');
     posts.push(new Post(post.val()));
     post.val("");
-}
 
-const REMOVE_BUTTON_HTML ="<button class='btn btn-primary remove' type='button'>Remove</button>";
-const COMMENT_DIV = "<div class='comment-box'></div>";
-const COMMENT_INPUT = '<input type="text" class="form-control comment-content" placeholder="Comment">';
-const COMMENT_USER = '<input type="text" class="form-control comment-user" placeholder="userName">';
-const NEW_LINE ="<BR>";
-const COMMENT_BUTTON = "<button class='btn btn-primary comment' type='button'>Comment</button>";
+}
+function addCommentClicked(i) {
+    posts[i].addComment($(this).siblings(".comment-user").val(), $(this).siblings(".comment-content").val());
+    render();
+
+}
+function removePost(i) {
+    posts.splice(i, 1);
+    render();
+
+}
+function removeComment(i, comment_i) {
+    posts[i].comments.splice(comment_i, 1);
+    render();
+
+}
 function render() {
+
     let postsDiv = $('.posts');
     postsDiv.empty();
 
-    for (let i = 0; i<posts.length; i++) {
+    for (let i = posts.length-1; i >= 0 ; i--) {
         // create post <p>
-        let post = $("<p class='post' data-id='"+posts[i].id+"'>"+ posts[i].text +"</p>");
+        let post = $("<p class='post' data-id='" + posts[i].id + "'>" + posts[i].text + "</p>");
         // create button
         let removeButton = $(REMOVE_BUTTON_HTML);
-        let userName = $(COMMENT_USER);
-        let commentBox = $(COMMENT_DIV).append(userName).append(COMMENT_INPUT).append(COMMENT_BUTTON);
+        let commentButton = $(COMMENT_BUTTON);
+
+        // assign add comment listener
+        commentButton.click(function () {addCommentClicked.call(this, i)});
+
+        let commentBox = $(COMMENT_DIV);
+        for (let comment_i = 0; comment_i < posts[i].comments.length; comment_i++) {
+            let name = $(COMMENTS_NAME).append(posts[i].comments[comment_i].name);
+            let comment = $(COMMENTS_CONTENT).append(posts[i].comments[comment_i].comment);
+
+            let remove = $(REMOVE_COMMENT).click(function () {removeComment(i, comment_i)});
+
+            let row = $(ROW).append(name).append(comment).append(remove);
+            commentBox.append(row);
+        }
+
+
+
         // append button to post
-        post.append(removeButton).append(NEW_LINE).append(commentBox);
+        post.append(removeButton).append(ROW).append(commentBox);
         // append post to all posts div
         postsDiv.append(post);
+        postsDiv.append($(ROW).append(COMMENT_USER).append(COMMENT_INPUT).append(commentButton));
 
         // on click remove from array and render again.
-        removeButton.click(function(){
-            posts.splice(i,1);
-            render();
-        })
+        removeButton.click(function () {removePost(i)});
     }
 }
 
-function onCreatePostClick() {
-    createPost();
-    render();
-}
-
+// add listenr to the add post button click event
 $('.add-post').click(onCreatePostClick);
